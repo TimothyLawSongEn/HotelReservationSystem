7ce9909e2b38fd7a045e171fcded04568a540e58
clara <clara@192.168.68.51>
2024-10-17 18:32:58 +0800

    added employee entity class and session beans

---
+++ HotelReservationSystem-ejb/src/java/ejb/session/stateless/EmployeeEntitySessionBeanLocal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/SessionLocal.java to edit this template
+ */
+package ejb.session.stateless;
+
+import entity.Employee;
+import java.util.List;
+import javax.ejb.Local;
+import javax.validation.ConstraintViolationException;
+
+/**
+ *
+ * @author clara
+ */
+@Local
+public interface EmployeeEntitySessionBeanLocal {
+
+    public Employee createEmployee(Employee newEmployee) throws ConstraintViolationException;
+
+    public Employee.EmployeeType logIn(Long employeeId, String password);
+
+    public List<Employee> viewAllEmployees();
+    
+}
