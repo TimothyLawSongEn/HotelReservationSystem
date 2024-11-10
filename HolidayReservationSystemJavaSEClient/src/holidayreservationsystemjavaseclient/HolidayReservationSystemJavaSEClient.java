bbef9bbcd1a18fe6a353a91c0f677ca407d3b682
Clara Lum <clara@Claras-MacBook-Pro.local>
2024-11-10 16:11:56 +0800

    feat: call web service in holiday.com

--- HolidayReservationSystemJavaSEClient/src/holidayreservationsystemjavaseclient/HolidayReservationSystemJavaSEClient.java
+++ HolidayReservationSystemJavaSEClient/src/holidayreservationsystemjavaseclient/HolidayReservationSystemJavaSEClient.java
@@ -1,3 +1,9 @@
+package holidayreservationsystemjavaseclient;
+
+import java.util.List;
+import ws.hors.Booking;
+import ws.hors.HotelReservationSystemWebService_Service;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -13,7 +19,12 @@ public class HolidayReservationSystemJavaSEClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        HotelReservationSystemWebService_Service service = new HotelReservationSystemWebService_Service();
+        List<Booking> bookings = service.getHotelReservationSystemWebServicePort().viewAllReservations();
+        
+        for (Booking b:bookings) {
+            System.out.println("Start Date: " + b.getStartDate() + "\nEnd Date: " + b.getEndDate() + "\nRoom Type: " + b.getRoomType());
+        }
+    } 
     
 }
