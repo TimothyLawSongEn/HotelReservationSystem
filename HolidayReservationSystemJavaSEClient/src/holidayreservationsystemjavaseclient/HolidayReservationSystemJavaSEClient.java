5d96a3cdc48e268a402a8ce8b0d9c03d919bb4e3
Clara Lum <clara@Claras-MacBook-Pro.local>
2024-11-12 19:42:00 +0800

    chore: refactor xml files to reflect the change of Guest entity to Account Entity

--- HolidayReservationSystemJavaSEClient/src/holidayreservationsystemjavaseclient/HolidayReservationSystemJavaSEClient.java
+++ HolidayReservationSystemJavaSEClient/src/holidayreservationsystemjavaseclient/HolidayReservationSystemJavaSEClient.java
@@ -1,17 +1,14 @@
-package holidayreservationsystemjavaseclient;
-
-import java.util.List;
-import ws.hors.Booking;
-import ws.hors.HotelReservationSystemWebService_Service;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package holidayreservationsystemjavaseclient;
+
+import ws.hors.HotelReservationSystemWebService_Service;
 
 /**
  *
- * @author timothy
+ * @author clara
  */
 public class HolidayReservationSystemJavaSEClient {
 
@@ -20,11 +17,8 @@ public class HolidayReservationSystemJavaSEClient {
      */
     public static void main(String[] args) {
         HotelReservationSystemWebService_Service service = new HotelReservationSystemWebService_Service();
-        List<Booking> bookings = service.getHotelReservationSystemWebServicePort().viewAllReservations();
-        
-        for (Booking b:bookings) {
-            System.out.println("Start Date: " + b.getStartDate() + "\nEnd Date: " + b.getEndDate() + "\nRoom Type: " + b.getRoomType());
-        }
-    } 
+        MainApp mainApp = new MainApp(service);
+        mainApp.start();
+    }
     
 }
